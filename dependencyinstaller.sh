991ddf30df2d27ca9e8a24ac818f0613b51ab198
Garrett MacDonald <1791145+garrettian@users.noreply.github.com>
2020-07-29 01:01:24 +0800

    add git dep

--- dependencyinstaller.sh
+++ dependencyinstaller.sh
@@ -4,6 +4,7 @@ sudo apt-get update -y
 sudo apt-get install golang -y
 sudo apt install nginx -y
 sudo apt install certbot -y
+sudo apt install git -y
 sudo add-apt-repository ppa:certbot/certbot -y
 curl -sSL https://git.io/g-install | sh -s
 echo STARTING NEW SHELL TO LOAD G-INSTALL...
