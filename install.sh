781baf862b022192d925d20a847ee4ff6a642534
Perspekt <aj.prasad8@gmail.com>
2020-07-30 01:49:48 +0000

    Update for 0.5.0 Mainnet

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ read -p 'Enter domain name for node (ex: node01.test.com: ' DOMAIN
 
 echo Configuring for domain: $DOMAIN
 
-SEEDS=b3d86cd8ab4aa0cb9861cb795d8d154e685a94cf@seed1.testnet.pokt.network:20656,17ca63e4ff7535a40512c550dd0267e519cafc1a@seed2.testnet.pokt.network:21656,f99386c6d7cd42a486c63ccd80f5fbea68759cd7@seed3.testnet.pokt.network:22656
+SEEDS=03b74fa3c68356bb40d58ecc10129479b159a145@seed1.mainnet.pokt.network:20656,64c91701ea98440bc3674fdb9a99311461cdfd6f@seed2.mainnet.pokt.network:21656,18eaabef85c661344b640b74597c4973af707ccb@pocket-seed.simply-vc.com.mt:26656,8d0bbd044cff904b782e597162a4184622d3c1e6@poktseed100.chainflow.io:26656
 
 echo Seeds to be used: $SEEDS
 
@@ -13,7 +13,7 @@ go get github.com/pokt-network/pocket-core
 sudo apt-get update -y 
 sudo apt-get install libleveldb-dev build-essential -y
 cd go/src/github.com/pokt-network/pocket-core
-git checkout tags/RC-0.4.3
+git checkout tags/RC-0.5.0
 echo $GOPATH
 
 if [ -z "$GOPATH" ]
@@ -28,11 +28,12 @@ fi
 
 sudo go build -tags cleveldb -o $GOPATH/bin/pocket ./app/cmd/pocket_core/main.go
 sleep 2
-pocket start #creates config.json
-# Add kill process
+pocket start --mainnet #creates config.json
+PID=$(pgrep -f "pocket start")
+kill $PID
 sleep 2
 cd ~/.pocket/config/
-curl -O https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/testnet/genesis.json
+curl -O https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/mainnet/genesis.json
 # Add  Create Chains
 cd ~
 
